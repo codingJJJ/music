0c4f81578241e1a6e22c7430661cadc72f0327ec
psj <271533323@qq.com>
2022-10-18 23:48:00 +0800

    commit

--- src/components/player/index.tsx
+++ src/components/player/index.tsx
@@ -1,8 +1,21 @@
-const Player = () => {
-  const {} = usePlayer();
+import React from "react";
+import type { FC } from "react";
+import usePlayer from "./usePlayer";
+
+type PlayerProps = {
+  next: () => void;
+  pre: () => void;
+  url: string;
+};
+const Player: FC<PlayerProps> = ({ next, pre, url }) => {
+  const { src, audioRef } = usePlayer(url);
+  return (
+    <div>
+      <audio ref={audioRef} src={src}>
+        当前版本不支持该浏览器
+      </audio>
+    </div>
+  );
 };
 
 export default Player;
-function usePlayer(): {} {
-  throw new Error("Function not implemented.");
-}
--- src/components/player/usePlayer.ts
+++ src/components/player/usePlayer.ts
@@ -2,15 +2,21 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import { useRef } from "react";
-const usePlayer = (src, el) => {
+const usePlayer = (url: string) => {
   const audioRef = useRef<HTMLAudioElement>(null);
-  const [playing, setPlaying] = useState(false);
-  const [volume, setVolume] = useState(1);
+  const [playing, setPlaying] = useState<boolean>(false);
+  const [volume, setVolume] = useState<number>(0.75);
 
   useEffect(() => {}, []);
 
   return {
     playing,
     setPlaying,
+    volume,
+    setVolume,
+    audioRef,
+    src: url,
   };
 };
+
+export default usePlayer;
